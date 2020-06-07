ad6cc83a84776e3325a96eb4505217aab74bd8ba
Grzegorz Caban <nabacg@gmail.com>
2020-06-07 18:41:41 +0100

    Parser rework to enable keyword access for keywords with '-' or '_' (so not alpha1), solution still feels like a bit of hack (reusing implementation of alpha1! ) but solves the problem for now. Also removed some dead code

--- src/parser.rs
+++ src/parser.rs
@@ -1,19 +1,19 @@
 use nom::{
   IResult,
-  sequence::{delimited, tuple},
-  bytes::complete::{tag, is_not, take_while1, take_while_m_n},
+//   sequence::{delimited, tuple},
+  bytes::complete::{tag, is_a},
   combinator::{map_res, map},
-  // see the "streaming/complete" paragraph lower for an explanation of these submodules
-  character::complete::char
 };
-use nom::multi::separated_list;
-use nom::character::complete::{alpha1, digit1};
+use nom::bytes::complete::take_while;
+use nom::multi::{separated_list, many1};
+use nom::character::is_alphanumeric;
+use nom::character::complete::{alpha0, alpha1, digit1, char, anychar};
 use nom::branch::alt;
-use nom::whitespace;
+use nom::error::{ErrorKind, ParseError};
+use nom::{ InputTakeAtPosition, AsChar};
 
 #[derive(Debug, Eq)]
 pub enum QueryCmd {
-    Unknown,
     MultiArrayIndex(Vec<usize>),
     KeywordAccess(Vec<String>),
     MultiCmd(Vec<QueryCmd>)
@@ -23,7 +23,6 @@ pub enum QueryCmd {
 impl PartialEq for QueryCmd {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
-            (QueryCmd::Unknown, QueryCmd::Unknown) => true,
             //(QueryCmd::SingleArrayIndex(i), QueryCmd::SingleArrayIndex(j)) => i == j,
             (QueryCmd::MultiArrayIndex(xs), QueryCmd::MultiArrayIndex(ys)) => xs == ys,
             (QueryCmd::KeywordAccess(xs), QueryCmd::KeywordAccess(ys)) => xs == ys,
@@ -40,33 +39,29 @@ impl PartialEq for QueryCmd {
 ///
 // let is_num = take_while1(is_digit)
 //https://stackoverflow.com/questions/54735672/how-do-i-use-nom-to-parse-a-string-with-sign-into-an-i32
-fn int_list(s: &str) -> IResult<&str, Vec<usize>> {
-    separated_list(tag(","), map_res(digit1, |s: &str| s.parse::<usize>()))(s)
-}
-
-fn string_list(s: &str) -> IResult<&str, Vec<String>> {
-    separated_list(tag("."), map(alpha1, |s: &str| s.to_string()))(s)
-}
+// fn int_list(s: &str) -> IResult<&str, Vec<usize>> {
+//     separated_list(tag(","), map_res(digit1, |s: &str| s.parse::<usize>()))(s)
+// }
 
 
-fn single_int(s: &str) -> IResult<&str, usize> {
-    map_res(digit1, |s: &str| s.parse::<usize>())(s)
-}
+// fn single_int(s: &str) -> IResult<&str, usize> {
+//     map_res(digit1, |s: &str| s.parse::<usize>())(s)
+// }
 
-fn array_index_access(s: &str) -> IResult<&str, QueryCmd> {
-    let (input, (_, xs, _)) = tuple((char('['), int_list, char(']')))(s)?;
-    Ok((input, QueryCmd::MultiArrayIndex(xs)))
-}
+// fn array_index_access(s: &str) -> IResult<&str, QueryCmd> {
+//     let (input, (_, xs, _)) = tuple((char('['), int_list, char(']')))(s)?;
+//     Ok((input, QueryCmd::MultiArrayIndex(xs)))
+// }
 
-fn keyword_access(s: &str) -> IResult<&str, QueryCmd> {
-    let (input, (_, xs, _)) = tuple((char('{'), string_list, char('}')))(s)?;
-    Ok((input, QueryCmd::KeywordAccess(xs)))
-}
+// fn keyword_access(s: &str) -> IResult<&str, QueryCmd> {
+//     let (input, (_, xs, _)) = tuple((char('{'), string_list, char('}')))(s)?;
+//     Ok((input, QueryCmd::KeywordAccess(xs)))
+// }
 
-fn multi_cmd_list(s: &str) -> IResult<&str, QueryCmd> {
-    let (input, cmds) = separated_list(tag("|"), alt((array_index_access, keyword_access)))(s)?;
-    Ok((input, QueryCmd::MultiCmd(cmds)))
-}
+// fn multi_cmd_list(s: &str) -> IResult<&str, QueryCmd> {
+//     let (input, cmds) = separated_list(tag("|"), alt((array_index_access, keyword_access)))(s)?;
+//     Ok((input, QueryCmd::MultiCmd(cmds)))
+// }
 
 // Nom macros https://github.com/Geal/nom/blob/master/doc/how_nom_macros_work.md
 
@@ -78,23 +73,40 @@ fn multi_cmd_list(s: &str) -> IResult<&str, QueryCmd> {
 //     )))
 //  )
 // );
+fn alpha_or_spec_char(input: &str) -> IResult<&str, &str>
+{
+    input.split_at_position1_complete(|item| !(item.is_alpha() || item == '-' || item == '_'), ErrorKind::Alpha)
+}
 
-named!(keyword_access_mac(&str) -> QueryCmd, map!(ws!(tuple!(tag!("{"), call!(string_list), tag!("}"))), |(_, ks, _)| QueryCmd::KeywordAccess(ks)));
+fn string_list(s: &str) -> IResult<&str, Vec<String>> {
+    separated_list(tag("."), map(alpha_or_spec_char, |s: &str| s.to_string()))(s)
+}
+//     separated_list(tag("."), map(
+//         //(take_while(|c| is_alphanumeric(c) || is_a("_")(c) || is_a("-")(c) )
+//         many1(alt(
+//             (alpha0, 
+//                 alt(
+//                     (tag("_"), 
+//                     tag("-")))
+//                 )))
+// , |s: &str| s.to_string()))(s)
 
-//separated_list(tag(","), map_res(digit1, |s: &str| s.parse::<usize>()))(s)
-named!(int_list_mac(&str) ->  Vec<usize>,  ws!(separated_list!(tag(","), map_res(digit1, |s: &str| s.parse::<usize>()))));
 
-named!(array_index_access_mac(&str) -> QueryCmd, map!(ws!(tuple!(tag!("["), call!(int_list_mac), tag!("]"))), |(_, ids, _)| QueryCmd::MultiArrayIndex(ids)));
+named!(keyword_access(&str) -> QueryCmd, map!(ws!(tuple!(tag!("{"), call!(string_list), tag!("}"))), |(_, ks, _)| QueryCmd::KeywordAccess(ks)));
 
-named!(multi_cmd_list_mac( &str) -> QueryCmd, 
+named!(int_list(&str) ->  Vec<usize>,  ws!(separated_list!(tag(","), map_res(digit1, |s: &str| s.parse::<usize>()))));
+
+named!(array_index_access(&str) -> QueryCmd, map!(ws!(tuple!(tag!("["), call!(int_list), tag!("]"))), |(_, ids, _)| QueryCmd::MultiArrayIndex(ids)));
+
+named!(multi_cmd_list( &str) -> QueryCmd, 
     map!(ws!(separated_list!(tag("|"),  
-     alt((array_index_access_mac,keyword_access_mac)))),
+     alt((array_index_access,keyword_access)))),
       |cmds| QueryCmd::MultiCmd(cmds)));
 
 
-named!(taggy_tags(&str) -> &str,  ws!(tag!(",")));
+// named!(taggy_tags(&str) -> &str,  ws!(tag!(",")));
 
-named!(int_mac(&str) ->  usize,  ws!( map_res!(digit1, |s: &str| s.parse::<usize>())));
+// named!(int_mac(&str) ->  usize,  ws!( map_res!(digit1, |s: &str| s.parse::<usize>())));
 
 
 // combinator list
@@ -107,7 +119,7 @@ pub fn parse(input: &str) -> IResult<&str, QueryCmd> {
 
     //alt((array_index_access, keyword_access))(input)
     //multi_cmd_list(input)
-    multi_cmd_list_mac(input)
+     multi_cmd_list(input)
 
 }
 
@@ -123,23 +135,6 @@ mod parser_tests {
         assert_eq!(int_list(&"1"),     Ok(("", vec![1])));
     }
 
-
-    #[test]
-    fn taggy_tag_test() {
-        assert_eq!(taggy_tags(&"  ,  "),      Ok(("", ",")));
-        assert_eq!(taggy_tags(&","),      Ok(("", ",")));
-
-        assert_eq!(int_mac(&"123"),      Ok(("", 123)));
-        assert_eq!(int_mac(&"  123  "),      Ok(("", 123)));
-    }
-
-    #[test]
-    fn int_list_mac_test() {
-        assert_eq!(int_list_mac(&""),      Ok(("", vec![])));
-
-        assert_eq!(int_list_mac(&"1,2"),     Ok(("", vec![1,2])));
-        assert_eq!(int_list_mac(&" 1 ,     2, 3"), Ok(("", vec![1,2,3])));
-    }
 
 
     #[test]
@@ -157,18 +152,21 @@ mod parser_tests {
         assert_eq!(keyword_access(&"{abc.def.ghi}"),     Ok(("", QueryCmd::KeywordAccess(vec!["abc".to_string(), "def".to_string(), "ghi".to_string()]))));
         assert_eq!(keyword_access(&"{TotalDuplicateImpressionBucketClicks}"),     Ok(("", QueryCmd::KeywordAccess(vec!["TotalDuplicateImpressionBucketClicks".to_string()]))));
        
+
+        assert_eq!(keyword_access(&"{abc-e.edf_g}"),     Ok(("", QueryCmd::KeywordAccess(vec!["abc-e".to_string(), "edf_g".to_string()]))));
+       
     }
 
 
 
     #[test]
     fn multi_cmd_list_test() {
-        assert_eq!(multi_cmd_list_mac(&"{aaa}"), Ok(("", QueryCmd::MultiCmd(vec![
+        assert_eq!(multi_cmd_list(&"{aaa}"), Ok(("", QueryCmd::MultiCmd(vec![
             QueryCmd::KeywordAccess(vec!["aaa".to_string()])
             ]))));
-        assert_eq!(multi_cmd_list_mac(&"[0]"), Ok(("", QueryCmd::MultiCmd(vec![
+        assert_eq!(multi_cmd_list(&"[0]"), Ok(("", QueryCmd::MultiCmd(vec![
             QueryCmd::MultiArrayIndex(vec![0])]))));
-        assert_eq!(multi_cmd_list_mac(&"[0] | {abc}"), Ok(("", QueryCmd::MultiCmd(vec![
+        assert_eq!(multi_cmd_list(&"[0] | {abc}"), Ok(("", QueryCmd::MultiCmd(vec![
                                                                 QueryCmd::MultiArrayIndex(vec![0]), 
                                                                 QueryCmd::KeywordAccess(vec!["abc".to_string()])
                                                                 ]))));
@@ -179,4 +177,6 @@ mod parser_tests {
                                                                 QueryCmd::MultiArrayIndex(vec![0]), 
                                                                 QueryCmd::KeywordAccess(vec!["abc".to_string()])]))));
     }
+
+
 }
