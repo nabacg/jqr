731f49af8091014399bba0afa9ed96ac4c111273
Grzegorz Caban <nabacg@gmail.com>
2020-12-14 20:08:48 +0000

    very ugly draft of groupBy, but including some neat refactoring that will allow cmds with delay evaluation of args

--- src/lib.rs
+++ src/lib.rs
@@ -83,6 +83,16 @@ fn print_json(val: &Value) {
     }
 }
 
+fn eval_sub_cmds(val: &Value, arg_cmds: Vec<QueryCmd>) -> Vec<Value> {
+    let mut res: Vec<Value> = Vec::new();
+
+    for cmd in arg_cmds {
+        let r = eval(val.clone(), cmd); // ToDo this needs fixing this cloning
+        res.push(r);
+    }
+    res
+}
+
 // ideally query should be an immutable ref, i.e. &QueryCmd but then we can't pattern match on both (json, query) because of Rust reasons..
 // but it would be great to solve it to avoid cloing QueryCmd on each recursive call
 // ToDo - could I use lifetimes to avoid cloning QueryCmd here?
@@ -170,14 +180,7 @@ fn eval(json: Value, query: QueryCmd) -> Value {
             }
         }
         (json, QueryCmd::FunCallCmd(fun_name, arg_cmds)) => {
-            let mut res: Vec<Value> = Vec::new();
-
-            for cmd in arg_cmds {
-                let r = eval(json.clone(), cmd); // ToDo this needs fixing this cloning
-                res.push(r);
-            }
-
-            function_registry_lookup(&fun_name, res)
+            function_registry_lookup(&fun_name, json, arg_cmds)
         }
         (json, QueryCmd::MultiCmd(cmds)) => {
             let mut val = json;
@@ -202,7 +205,8 @@ fn json_to_num(v: &Value) -> f64 {
     }
 }
 
-fn flatten_json_to_num_array(vs: &Vec<Value>) -> Vec<f64> {
+fn flatten_json_to_num_array(json: Value, cmds: Vec<QueryCmd>) -> Vec<f64> {
+    let vs: Vec<Value> = eval_sub_cmds(&json, cmds);
     let res: Vec<f64> =
      vs.iter().flat_map(|v| match v {
         Value::Array(vs) => vs.iter().map(|v| json_to_num(&v)).collect(),
@@ -212,53 +216,125 @@ fn flatten_json_to_num_array(vs: &Vec<Value>) -> Vec<f64> {
     res
 }
 
-fn sum_json_nums(json_nums: &Vec<Value>) -> Value {
-    let res = flatten_json_to_num_array(&json_nums).iter().fold(0.0, |acc, n| acc + n);
+fn sum_json_nums(json: Value, cmds: Vec<QueryCmd>) -> Value {
+    let res = flatten_json_to_num_array(json, cmds).iter().fold(0.0, |acc, n| acc + n);
     json!(res)
 }
 
-fn max_json_nums(nums : &Vec<Value>) -> Value {
-    let mut nums = flatten_json_to_num_array(&nums);
+fn max_json_nums(json: Value, cmds: Vec<QueryCmd>) -> Value {
+    let mut nums = flatten_json_to_num_array(json, cmds);
     // note a, b flipped, so it's a DESC sort
     nums.sort_by(|a, b| b.partial_cmp(a).unwrap());
     json!(nums[0])   
 }
 
-fn min_json_nums(nums : &Vec<Value>) -> Value {
-    let mut nums = flatten_json_to_num_array(&nums);
+fn min_json_nums(json: Value, cmds: Vec<QueryCmd>) -> Value {
+    let mut nums = flatten_json_to_num_array(json, cmds);
     nums.sort_by(|a, b| a.partial_cmp(b).unwrap());
    json!(nums[0])
 }
 
-fn avg_json_nums(json_nums: &Vec<Value>) -> Value {
-    let nums = flatten_json_to_num_array(&json_nums);
+fn avg_json_nums(json: Value, cmds: Vec<QueryCmd>) -> Value {
+    let nums = flatten_json_to_num_array(json, cmds);
     let sum = nums.iter().fold(0.0, |acc, v| acc + v);
     println!("sum = {}, len = {}, avg = {}", sum,  nums.len(), sum / (nums.len() as f64));
     json!((sum / (nums.len() as f64)))
 }
 
-fn sort_json_nums(json_nums : &Vec<Value>) -> Value {
-    let mut nums = flatten_json_to_num_array(json_nums);
+fn sort_json_nums(json: Value, cmds: Vec<QueryCmd>) -> Value {
+    let mut nums = flatten_json_to_num_array(json, cmds);
     nums.sort_by(|a, b| a.partial_cmp(b).unwrap());
     json!(nums)
+}
+
+fn group_by_cmd(json:Value, cmds:Vec<QueryCmd>) -> Value {
+    if cmds.len() != 2 {
+        panic!("groupBy can only be called with 2 arguments, a value to group and a expression to it groupBy")
+    }
+
+    let cmd_to_group = cmds[0].clone();
+    let cmd_to_group_by = cmds[1].clone();
+
+    let value_to_group = eval(json, cmd_to_group);
+
+    match value_to_group {
+        Value::Array(values) => {
+            // extract a function here, similar code in eval branch   (json, QueryCmd::TransformIntoObject(prop_mapping)) 
+        
+          //  let mut group_dict: HashMap<String, >
+
+            let mut props: Map<String, Value> = Map::new();
+            // there has to be a better way to do this, not only does the code look horrible 
+            // but the performance seems to be really bad
+            // below groupBy takes 60secs !!
+            //  58.60s user 0.60s system 99% cpu 59.393 total
+            //time cargo run 004ff2c5-7ed0-433b-8638-e6ceeceb1d09-7 "Records | [0] | groupBy(Details, Grouping.CampaignId) | .keys  "
+//             [
+//   "1337887",
+//   "1382752",
+//   "1438498",
+//   "1441118",
+//   "1455062",
+//   "1490109",
+//   "1491444",
+//   "1495891",
+//   "1496524",
+//   "1496561",
+//   "1498116",
+//   "1502539",
+//   "1502608",
+//   "548278",
+//   "608806",
+//   "618697",
+//   "817940"
+// ]
+// cargo run 004ff2c5-7ed0-433b-8638-e6ceeceb1d09-7  
+            // maybe use HashMap and mutable Vectors as vals ? https://doc.rust-lang.org/rust-by-example/std/hash.html
+            for json_val in values {
+                let group_key = eval(json_val.clone(), cmd_to_group_by.clone()); // Todo this cloning sucks! Can I do lifetimes to limit this?
+                let key_string = serde_json::to_string(&group_key).unwrap();
+                match props.get(&key_string) {
+                    Some(Value::Array(vs)) =>  { 
+
+                        let mut new_vec = vs.clone();
+                        new_vec.push(json_val);
+                        props.insert(key_string, json!(new_vec));
+                        
+                        }
+                    None => { props.insert(key_string, json!(vec![json_val])); }
+                    v => panic!("Group by return value adding to previous group should always find a Vec<Value>, but found something else: {:?}", v)
+                }
+                
+            }
+            json!(props)
+        }
+        json_val => {
+            let group_key = eval(json_val.clone(), cmd_to_group_by.clone());
+            let key_string = serde_json::to_string(&group_key).unwrap();
+            json!({key_string: json_val})
+
+        }  
+               
+    }
 }
 
 // Apparently you need to impl Fn trait https://stackoverflow.com/a/38947708
 //-> Vec<Value> -> Value {
 // ((Vec<Value>) -> Value)
-fn function_registry_lookup(fn_name: &str, vs: Vec<Value>) -> Value {
+fn function_registry_lookup(fn_name: &str, json: Value, cmds: Vec<QueryCmd>) -> Value {
     match fn_name {
-        "sum" => sum_json_nums(&vs),
-        "max" => max_json_nums(&vs),
-        "min" => min_json_nums(&vs),
-        "avg" => avg_json_nums(&vs),
-        "sort" => sort_json_nums(&vs),
+        "sum"  => sum_json_nums(json, cmds),
+        "max"  => max_json_nums(json, cmds),
+        "min"  => min_json_nums(json, cmds),
+        "avg"  => avg_json_nums(json, cmds),
+        "sort" => sort_json_nums(json, cmds),
+        "groupBy" => group_by_cmd(json, cmds),
         _ => panic!(format!("Function {}! not supported yet!", fn_name)),
     }
 }
 
 fn eval_inner(json: Value, query: QueryCmd) {
-    // println!("command found = {:?}", query);
+     println!("command found = {:?}", query);
     let res_json = eval(json, query);
     print_json(&res_json)
 }
