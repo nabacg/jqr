0d3d1aeaeeba8e8204c073a3587760cb29640fca
Grzegorz Caban <nabacg@gmail.com>
2021-11-06 16:45:05 +0100

    adding MultiCmd, plus some better WHITESPACE handling in other rules

--- src/parser.rs
+++ src/parser.rs
@@ -56,24 +56,14 @@ fn parse_err(msg: &str) -> Box<dyn Error> {
     String::from(msg).into()
 }
 
-pub fn parse(input: &str) -> Result<QueryCmd, Box<dyn Error>> {
-    let mut parsed = JQRParser::parse(Rule::jqExpr, input)?;
-    let parse_res = parsed.next().unwrap();
-
-
-    let err: Box<dyn Error> = String::from("Empty top level parse result").into();
-    let expr = parse_res.into_inner().next().ok_or(err)?;
-    println!("parsed: {:?}, expr: {:?}, rule: {:?}", parsed, expr, expr.as_rule());
-    match expr.as_rule() {
+fn parse_expr(expr: Pair<Rule>) -> Result<QueryCmd, Box<dyn Error>> {
+      match expr.as_rule() {
         Rule::valsExpr  => Ok(QueryCmd::ListValues),
         Rule::keysExpr  => Ok(QueryCmd::ListKeys),
         Rule::countExpr => Ok(QueryCmd::Count),
         Rule::multiKeyword =>  {
             parse_keyword(expr)
-            // let kws: Vec<String> = expr.into_inner().map(|kw| kw.as_str().to_string()).collect();
-            // println!("kws: {:?}", kws);
-            // Ok(QueryCmd::KeywordAccess(kws))
-        },        
+        },
         Rule::indexAccess => {
             let idx: Vec<usize> = expr.into_inner().map(|kw| kw.as_str().parse::<usize>().unwrap()).collect();
             Ok(QueryCmd::ArrayIndexAccess(idx))
@@ -83,14 +73,28 @@ pub fn parse(input: &str) -> Result<QueryCmd, Box<dyn Error>> {
             let query_expr = expr.next().ok_or(parse_err("filterExpr - invalid queryExpr"))?;
             let val_expr   = expr.next().ok_or(parse_err("filterExpr - invalid valueExpr"))?;
             let unquoted_val = val_expr.into_inner().next().ok_or(parse_err("valueExpr - expected quoted value"))?;
-            
-            
-            Ok(QueryCmd::FilterCmd(Box::new(parse_keyword(query_expr)?), unquoted_val.as_str().to_string()))
-            
-        },
 
+
+            Ok(QueryCmd::FilterCmd(Box::new(parse_keyword(query_expr)?), unquoted_val.as_str().to_string()))
+
+        },
+        Rule::multiExpr => {
+            let cmds = expr.into_inner().map(|expr| parse_expr(expr).expect("parseMulti failed"));
+            Ok(QueryCmd::MultiCmd(cmds.collect()))
+        },
         _ => unreachable!()
-    }
+      }
+}
+
+pub fn parse(input: &str) -> Result<QueryCmd, Box<dyn Error>> {
+    let mut parsed = JQRParser::parse(Rule::jqExpr, input)?;
+    let parse_res = parsed.next().unwrap();
+
+
+    let err: Box<dyn Error> = String::from("Empty top level parse result").into();
+    let expr = parse_res.into_inner().next().ok_or(err)?;
+    println!("parsed: {:?}, expr: {:?}, rule: {:?}", parsed, expr, expr.as_rule());
+    parse_expr(expr)
 }
 
 
@@ -98,30 +102,48 @@ pub fn parse(input: &str) -> Result<QueryCmd, Box<dyn Error>> {
 mod parser_test {
     use super::*;
 
+    fn run_parse(s: &str) -> QueryCmd {
+
+        parse(s).expect("Parse failed")
+    }
+
     #[test]
     fn parse_test() {
-        let exp_err = "Parse failed";
-        assert_eq!(parse("a").expect(exp_err), QueryCmd::KeywordAccess(vec!["a".to_string()]));
-        assert_eq!(parse("a.b.c").expect("OK"),
+        assert_eq!(run_parse("a"), QueryCmd::KeywordAccess(vec!["a".to_string()]));
+        assert_eq!(run_parse("a.b.c"),
                    QueryCmd::KeywordAccess(vec!["a".to_string(), "b".to_string(), "c".to_string()]));
 
-        assert_eq!(parse("a.b.c").expect("OK"),
+        assert_eq!(run_parse("a.b.c"),
                    QueryCmd::KeywordAccess(vec!["a".to_string(), "b".to_string(), "c".to_string()]));
 
-     //   assert_eq!(parse("a  .b .c").err().is_none(), false);
+        //assert_eq!(parse("a  .b .c").err().is_some(), true);
+        assert_eq!(run_parse("a .b   .c"),
+                   QueryCmd::KeywordAccess(vec!["a".to_string(), "b".to_string(), "c".to_string()]));
 
-        assert_eq!(parse("[0]").expect(exp_err), QueryCmd::ArrayIndexAccess(vec![0]));
+        assert_eq!(run_parse("[0]"), QueryCmd::ArrayIndexAccess(vec![0]));
         assert_eq!(parse("[]").err().is_some(), true);
-        assert_eq!(parse("[1,3, 5]").expect(exp_err), QueryCmd::ArrayIndexAccess(vec![1,3,5]));
+        assert_eq!(run_parse("[1,3, 5]"), QueryCmd::ArrayIndexAccess(vec![1,3,5]));
 
         assert_eq!(parse("[1,3, ea]").err().is_some(), true);
 
 
-        assert_eq!(parse(".vals").expect(exp_err), QueryCmd::ListValues);
-        assert_eq!(parse(".keys").expect(exp_err), QueryCmd::ListKeys);
-        assert_eq!(parse(".count").expect(exp_err), QueryCmd::Count);
+        assert_eq!(run_parse(".vals"), QueryCmd::ListValues);
+        assert_eq!(run_parse(".keys"), QueryCmd::ListKeys);
+        assert_eq!(run_parse(".count"), QueryCmd::Count);
 
-        assert_eq!(parse("username = \"Adam\"").expect(exp_err), QueryCmd::FilterCmd(Box::new(
+        assert_eq!(run_parse("username = \"Adam\""), QueryCmd::FilterCmd(Box::new(
             QueryCmd::KeywordAccess(vec!("username".to_string()))),  "Adam".to_string()));
+
+        assert_eq!(run_parse("address = \"P Sherman 42 Wallaby Way\""), QueryCmd::FilterCmd(Box::new(
+            QueryCmd::KeywordAccess(vec!("address".to_string()))),  "P Sherman 42 Wallaby Way".to_string()));
+
+        assert_eq!(run_parse("[230] | a.b"), QueryCmd::MultiCmd(vec![QueryCmd::ArrayIndexAccess(vec![230]),
+                                                                     QueryCmd::KeywordAccess(vec!["a".to_string(), "b".to_string()])]));
+
+        assert_eq!(run_parse("[230] | a.b | .vals"),
+                   QueryCmd::MultiCmd(vec![
+                       QueryCmd::ArrayIndexAccess(vec![230]),
+                       QueryCmd::KeywordAccess(vec!["a".to_string(), "b".to_string()]),
+                   QueryCmd::ListValues]));
     }
 }
